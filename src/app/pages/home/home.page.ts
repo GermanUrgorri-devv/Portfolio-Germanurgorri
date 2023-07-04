8de1ad6b5720636fd33cbd7bb38fdffd3e1a3b5b
Germanurgorri-devv <germanurgorri@gmail.com>
2023-07-04 05:15:34 +0100

    structure of page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
+import { Location } from '@angular/common';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  // Use ViewChild to get a reference to the IonContent element in the template
+  @ViewChild(IonContent, { static: false })
+  content!: IonContent;
 
-  constructor() { }
+  email?: String;
+  message?: String;
 
-  ngOnInit() {
+  constructor(
+    private location: Location,
+    private formBuilder: FormBuilder,
+    ) {}
+
+  public contactForm = this.formBuilder.group({
+    email: [''],
+    message: [''],
+  });
+
+  // Method that allows scrolling to a specific element on the page
+  scrollTo(elementId: string) {
+    let element = document.getElementById(elementId);
+    if (element !== null) {
+      let yOffset = element.offsetTop;
+      this.content.scrollToPoint(0, yOffset, 1000);
+    }
   }
 
+  ngOnInit() {
+
+  
+
+  }
+
+  
 }
